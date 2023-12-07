03b6547098c3b70ee8d16ecfcd5232273d8c3db0
samsepiolc64 <mjarmuzek@wp.pl>
2023-12-07 22:56:48 +0100

    day1_2

--- day1/adventofcode2023_1_2.py
+++ day1/adventofcode2023_1_2.py
@@ -3,26 +3,40 @@ import math
 path = 'day1/data.txt'
 mass_file = open(path,'r')
 
+cyfry_slownie = {
+    'one':'1',
+    'two':'2', 
+    'three':'3', 
+    'four':'4', 
+    'five':'5', 
+    'six':'6', 
+    'seven':'7', 
+    'eight':'8', 
+    'nine':'9'
+    }
+
 def first_line(data):
     sum = 0
     for line in data:
+        for key in cyfry_slownie.keys():
+            if line.find(key) != -1:
+                line = line.replace(key,cyfry_slownie[key])
+        print("------")
+        print(line)
         calib = ""
-        #print(line)
         for char in line.strip():
             if char.isdigit():
-                # print ('p' + char, end=' ')
                 calib += char
+                print("fir=" + char)
                 break
         for i in range(len(line.strip()) - 1, -1, -1):
             if line.strip()[i].isdigit():
-                #print ('o' + line.strip()[i])
                 calib += line.strip()[i]
+                print("ost=" + line.strip()[i])
                 break
         sum += int(calib)
+        print("------")
     return sum
-        
-                
-        
 
 
 if mass_file is not None:
