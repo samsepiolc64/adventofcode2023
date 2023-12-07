2ee2b5e7d32b384f6ef782efb15a024b85b14de6
samsepiolc64 <mjarmuzek@wp.pl>
2023-12-07 15:02:15 +0100

    day1_1 lines

--- day1/adventofcode2023_1_1.py
+++ day1/adventofcode2023_1_1.py
@@ -1,43 +1,22 @@
 import math
 
-path = 'data.txt'
+path = 'day1/data.txt'
 mass_file = open(path,'r')
-data = mass_file.read().splitlines()
 
-i = 1
-suma = 0
-ss=[12]
+def first_line(data):
+    for line in data:
+        for char in line.strip():
+            if char.isdigit():
+                print ('p' + char, end=' ')
+        for i in range(len(line.strip()) - 1, -1, -1):
+            if line.strip()[i].isdigit():
+                print ('o' + line.strip()[i])
+                
+        
 
-
-def pierwszy_wiersz(tekst):
-    wiersze = tekst.split('\n')
-    if wiersze:
-        return wiersze[0]
-    return None
-
-# def sumator(data):
-#     ss = []
-#     for i in data:
-#         s = math.floor(int(i)/3)-2
-#         if s <= 0:
-#             s = 0
-#         ss.append(s)
-#     return ss
-
-
-# while sum(ss) != 0:
-#     ss = sumator(data)
-#     suma += sum(ss)
-#     print('---')
-#     print(ss)
-#     print(suma)
-#     data = ss
-
-
-if data is not None:
-    print("Pierwszy wiersz:", data)
+if mass_file is not None:
+    print("Temp data", first_line(mass_file))
 else:
-    print("Brak wierszy w tekście.")
-
+    print("Data file is empty.")
 
 mass_file.close()
