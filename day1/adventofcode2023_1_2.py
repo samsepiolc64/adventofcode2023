89f92db2dfb42e23524ab77171d8a03d511a36b4
samsepiolc64 <mjarmuzek@wp.pl>
2023-12-08 17:23:09 +0100

    day2_1 finish

--- day1/adventofcode2023_1_2.py
+++ day1/adventofcode2023_1_2.py
@@ -4,6 +4,14 @@ path = 'day1/data.txt'
 mass_file = open(path,'r')
 
 cyfry_slownie = {
+    'oneight':'18',
+    'threeight':'38',
+    'fiveight':'58',
+    'sevenine':'79',
+    'eightree':'83',
+    'eightwo':'82',
+    'nineight':'98',
+    'twone':'21',
     'one':'1',
     'two':'2', 
     'three':'3', 
@@ -16,26 +24,29 @@ cyfry_slownie = {
     }
 
 def first_line(data):
+    count = 1
     sum = 0
     for line in data:
         for key in cyfry_slownie.keys():
             if line.find(key) != -1:
                 line = line.replace(key,cyfry_slownie[key])
-        print("------")
-        print(line)
+        #print(line)
         calib = ""
         for char in line.strip():
             if char.isdigit():
                 calib += char
-                print("fir=" + char)
+                #print("fir=" + char)
                 break
         for i in range(len(line.strip()) - 1, -1, -1):
             if line.strip()[i].isdigit():
                 calib += line.strip()[i]
-                print("ost=" + line.strip()[i])
+                #print("ost=" + line.strip()[i])
                 break
         sum += int(calib)
-        print("------")
+        #print(calib)
+        #print(count,  "-", sum)
+        #count += 1
+        #print("------")
     return sum
 
 
--- day2/adventofcode2023_2_1.py
+++ day2/adventofcode2023_2_1.py
@@ -1,43 +1,54 @@
-import math
+import os
 
-path = 'data.txt'
+path = 'day2/data.txt'
 mass_file = open(path,'r')
-data = mass_file.read().splitlines()
+datas = mass_file.read().splitlines()
 
+colors = ['red', 'green', 'blue']
+
+def packages(data):
+        red = 0
+        green = 0
+        blue = 0
+        game_count = 1
+        czesci = data.split(":", 1)
+        data = czesci[1].strip()
+        tablice = data.split(';')
+        tablice = [element.strip() for element in tablice]
+        slowniki_kolorow = []
+        for grupa in tablice:
+            parts = grupa.split(', ')
+            slownik_kolorow = {}
+            for part in parts:
+                ilosc, kolor = part.split()
+                slownik_kolorow[kolor] = int(ilosc)
+            slowniki_kolorow.append(slownik_kolorow)   
+        return slowniki_kolorow
+
+def key_sum(dane):
+    suma_kolorow = {}
+
+    for slownik in dane:
+        for klucz, wartosc in slownik.items():
+            suma_kolorow[klucz] = suma_kolorow.get(klucz, 0) + wartosc
+    if suma_kolorow.get('red') == 12 and suma_kolorow.get('green') == 13 and suma_kolorow.get('blue') == 14:
+         return true
+
+def key_check(dane):
+    coun = 0
+    for pack in dane:
+        if ((pack.get('red') is not None and pack.get('red') > 12) or (pack.get('green') is not None and pack.get('green') > 13) or (pack.get('blue') is not None and pack.get('blue') > 14)):
+            coun += 1
+    return coun
+
+os.system('cls' if os.name == 'nt' else 'clear')
 i = 1
-suma = 0
-ss=[12]
-
-
-def pierwszy_wiersz(tekst):
-    wiersze = tekst.split('\n')
-    if wiersze:
-        return wiersze[0]
-    return None
-
-# def sumator(data):
-#     ss = []
-#     for i in data:
-#         s = math.floor(int(i)/3)-2
-#         if s <= 0:
-#             s = 0
-#         ss.append(s)
-#     return ss
-
-
-# while sum(ss) != 0:
-#     ss = sumator(data)
-#     suma += sum(ss)
-#     print('---')
-#     print(ss)
-#     print(suma)
-#     data = ss
-
-
-if data is not None:
-    print("Pierwszy wiersz:", data)
-else:
-    print("Brak wierszy w tekście.")
-
+sum = 0
+for data in datas:
+    print (key_check(packages(data)) )
+    if key_check(packages(data)) < 1:
+        sum += i
+    i += 1
+print(sum)
 
 mass_file.close()
